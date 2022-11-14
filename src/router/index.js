63dda11dca86fbf336e9a8d2a5435c2769e9fbf0
yangang <yang@ktwelve.com.cn>
2022-11-14 13:45:30 +0800

    ✨ feat: 粒子波浪特效

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ const routes = [
   { path: '/', component: () => import('@/views/case1/index.vue')},
   { path: '/case2', component: () => import('@/views/case2/index.vue')},
   { path: '/case3', component: () => import('@/views/case3/index.vue')},
-  { path: '/case4', component: () => import('@/views/case4/index.vue')}
+  { path: '/case4', component: () => import('@/views/case4/index.vue')},
+  { path: '/case5', component: () => import('@/views/case5/index.vue')}
 ]
 
 export const router = createRouter({
--- src/views/case5/index.vue
+++ src/views/case5/index.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script setup>// @ts-nocheck
-import { onMounted } from 'vue'
+import { onBeforeUnmount, onMounted, reactive } from 'vue'
 import * as THREE from 'three'
+
+let scene, camera, renderer
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+let mouseX = 0, mouseY = 0
 
 onMounted(()=>{
   initThree()
+})
+
+onBeforeUnmount(()=>{
+  window.removeEventListener('resize', onWindowResize, false)
+  document.removeEventListener('mousemove', onPointerMove, false )
 })
 
 const initThree = ()=>{
@@ -21,24 +31,16 @@ const initThree = ()=>{
     AMOUNTY = 50
 
   let count = 0
-  let windowHalfX = window.innerWidth / 2;
-	let windowHalfY = window.innerHeight / 2;
-
+ 
   // 新建场景
-  const scene = new THREE.Scene()
+  scene = new THREE.Scene()
+  
 
   // 新建摄像机
-  const camera = new THREE.PerspectiveCamera(75, s_w / s_h, 1, 1000)
-  camera.position.set(0,0,1000)
+  camera = new THREE.PerspectiveCamera(75, s_w / s_h, 1, 10000)
+  camera.position.z = 1000;
+  camera.position.y = 1000;
   scene.add(camera)
-
-  // 新建渲染器
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl'),
-    antialias: true //抗锯齿
-  })
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))// 设置像素比
-  renderer.setSize(s_w, s_h)
 
   // 添加粒子
   const numParticles = AMOUNTX * AMOUNTY
@@ -46,14 +48,14 @@ const initThree = ()=>{
   const scales = new Float32Array( numParticles )
 
   let i = 0, j = 0
-  for(let ix = 0 ; i < AMOUNTX; ix ++){
-    for(let iy = 0; iy < AMOUNTY; iy ++){
-      positions[i] = ix * SEPARATION - ((AMOUNTX * SEPARATION) / 2)
-      positions[i+1] = 0
-      positions[i+2] = iy * SEPARATION - ((AMOUNTY * SEPARATION) / 2)
-      scales[j] = 1
-      i+=3
-      j++
+  for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+    for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+      positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ) // x
+      positions[ i + 1 ] = 0; // y
+      positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ) // z
+      scales[ j ] = 1
+      i += 3
+      j ++
     }
   }
   const geometry = new THREE.BufferGeometry()
@@ -62,7 +64,7 @@ const initThree = ()=>{
 
   const material = new THREE.ShaderMaterial({
     uniforms:{
-      color: {value: new THREE.Color(0xffffff)}
+      color: {value: new THREE.Color('#097bdb')}
     },
     vertexShader:`
       attribute float scale;
@@ -83,22 +85,30 @@ const initThree = ()=>{
   const particles = new THREE.Points(geometry, material)
   scene.add(particles)
 
+  // 新建渲染器
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl'),
+    antialias: true //抗锯齿
+  })
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))// 设置像素比
+  renderer.setSize(s_w, s_h)
+
   // 动画函数
   const animate = ()=>{
     requestAnimationFrame(animate)
     render()
   }
   const render = ()=>{
-    // camera.position.x += ( mouseX - camera.position.x ) * .05;
-    // camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y = 300;
     camera.lookAt( scene.position );
     const positions = particles.geometry.attributes.position.array;
     const scales = particles.geometry.attributes.scale.array;
     let i = 0, j = 0;
     for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
       for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
-        positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
-        scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 20 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 20;
+        positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 )
+        scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 20 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 20
         i += 3;
         j ++;
       }
@@ -111,14 +121,22 @@ const initThree = ()=>{
   animate()
 
   // 监听函数
-  const onWindowResizeHandler = () => {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-  }
-  window.addEventListener('resize', onWindowResizeHandler, false)
+ 
+  window.addEventListener('resize', onWindowResize, false)
+  document.addEventListener('mousemove', onPointerMove, false )
+}
+
+const onPointerMove = ( event )=> {
+  if ( event.isPrimary === false ) return;
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+}
+const onWindowResize = () => {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 </script>
